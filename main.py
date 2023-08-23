4b3c9edf18edd2307840d511695c20e75206f70d
sanjay321654 <sanjayilaya2002@gmail.com>
2023-08-23 23:35:52 +0530

    updated data.py,main.py,questions_model.py and quiz_brain.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-class User:
-    def __init__(self, user_id):
-        self.id = user_id
-user = User("000")
-print(user.id)
+from data import question_data
+from question_model import Question
+from quiz_brain import Quiz_brain
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+quiz = Quiz_brain(question_bank)
+
+while quiz.still_has_questions():
+
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
