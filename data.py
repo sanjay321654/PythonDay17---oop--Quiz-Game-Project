8760fdc13f61dcf37e6c83a053cc4d3fb9e0eacf
sanjay321654 <sanjayilaya2002@gmail.com>
2023-08-24 10:52:20 +0530

    updated data.py,main.py

--- data.py
+++ data.py
@@ -1,18 +1,40 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car,"
-             " you are free to take it home to eat.",
-     "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-     "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
 
+             {
+              "category": "General Knowledge",
+              "type": "boolean", "difficulty": "easy",
+              "question": "Bulls are attracted to the color red.",
+              "correct_answer": "True",
+              "incorrect_answers": ["False"]
+             },
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "The Sun rises from the North.", "correct_answer": "True",
+          "incorrect_answers": ["False"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "You can legally drink alcohol while driving in Mississippi.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "On average, at least 1 person is killed by a drunk driver in the United States every hour.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "Pluto is a planet.", "correct_answer": "False",
+          "incorrect_answers": ["True"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "One of Donald Trump&#039;s 2016 Presidential Campaign promises was to build a border wall between the United States and Mexico.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian. ",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+          "question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+          "correct_answer": "False", "incorrect_answers": ["True"]},
+         {
+             "category": "General Knowledge",
+             "type": "boolean", "difficulty": "easy",
+          "question": "A pasodoble is a type of Italian pasta sauce.",
+          "correct_answer": "False",
+             "incorrect_answers": ["True"]}
+]
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from question_model import Question
 from quiz_brain import Quiz_brain
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 quiz = Quiz_brain(question_bank)
